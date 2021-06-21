9553a28d6673f329462237380067c7a90db48cb7
jdwalsh138 <84231343+jdwalsh138@users.noreply.github.com>
2021-06-22 02:05:55 +1000

    Update script.js

--- script.js
+++ script.js
@@ -16,71 +16,25 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Prompts that come up after you click generate password
-function generatePassword() {
-  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
-
-  var numbers = confirm("Do you want numbers in your password?");
-
-  var lowerCases = confirm("Do you want lowercases in your password?");
-
-  var upperCases = confirm("Do you want uppercases in your password?");
-
-  var special = confirm("Do you want special characters in your password?");
-
   // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
   var minimumCount = 0;
 
 
   // Empty minimums for numbers, lowerCases, upperCases & specialCharacters
 
-  var minimumNumbers = "";
-  var minimumLowerCases = "";
   var minimumUpperCases = "";
-  var minimumSpecialCharacters = "";
 
 
   var functionArray = [
-    function getNumbers() {
-      return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-    },
-
-    function getLowerCases() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-    },
-
     function getUpperCases() {
       return +String.fromCharCode(Math.floor(Math.random() * 26 + 65));
     },
-
-    function getSpecialCharacters() {
-      return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
-    }
-
   ];
 
   // Checks to make sure user selected ok for all and uses empty minimums from above
 
-  if (numbers === true) {
-    minimumNumbers = functionArray[0];
-    minimumCount++;
-
-  }
-
-  if (lowerCases === true) {
-    minimumLowerCases = functionArray[1];
-    minimumCount++;
-
-  }
-
-  if (upperCases === true) {
+if (upperCases === true) {
     minimumUpperCases = functionArray[2];
-    minimumCount++;
-
-  }
-
-  if (special === true) {
-    minimumSpecialCharacters = functionArray[3];
     minimumCount++;
 
   }
@@ -97,10 +51,7 @@ function generatePassword() {
   }
 
   // to make sure characters are added to the password
-  randomPasswordGenerated += minimumNumbers;
-  randomPasswordGenerated += minimumLowerCases;
   randomPasswordGenerated += minimumUpperCases;
-  randomPasswordGenerated += minimumSpecialCharacters;
 
 
   return randomPasswordGenerated;
