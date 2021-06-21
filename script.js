071a29c9a9a00e202aadb296249942db4ba3e148
jdwalsh138 <84231343+jdwalsh138@users.noreply.github.com>
2021-06-21 23:13:58 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const characterAmountRange = document.getElementById
+('characterAmountRange')
+const characterAmountNumber = document.getElementById
+('characterAmountNumber')
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
