f868de284d71d303c319445cf58238016f09fc5c
jdwalsh138 <84231343+jdwalsh138@users.noreply.github.com>
2021-06-22 03:35:53 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = "!@#$%^&*()";
 
-const specialCharacters = "!@#$%^&*()";
-
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,6 +11,7 @@ function writePassword() {
 
 }
 
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
