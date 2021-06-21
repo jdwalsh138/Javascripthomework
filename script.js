03b3a7bc654298a215508c0f362ac681d4b32455
jdwalsh138 <84231343+jdwalsh138@users.noreply.github.com>
2021-06-22 01:51:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -10,34 +10,5 @@ function writePassword() {
 
 }
 
-function generatePassword() {
-  var characterAmountRange = prompt;
-  var characterAmountNumber = prompt;
-  var includeUppercase = confirm;
-  var minimumCount = 0;
-  var minimumUpperCases = "";
-}
-
- var functionArray = {
-     includeUppercase: function() {
-      return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-}
-
-   if (minimumUpperCases === true) {
-    minimumUpperCases = functionArray.includeUpperCases();
-    minimumCount++;
-}
-
-var randomPasswordGenerated = "";
-
-for (let i = 0; i < (parseInt(characterAmountRange) - minimumCount); i++) {
-    var randomNumberPicked = Math.floor(Math.random() * 4);
-
-    randomPasswordGenerated += randomNumberPicked;
-  
-return randomPasswordGenerated;
-  
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
